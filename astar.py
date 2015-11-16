8f831a2f8863e18fe25acae07b015ed7b76225de
Mateus Amarante Araujo <mamarantearaujo@AK120D-04.wpi.edu>
2015-11-15 20:45:31 -0500

    Compiling

--- astar.py
+++ astar.py
@@ -2,9 +2,10 @@
 
 import rospy, math
 
-from nav_msgs.msg import OccupancyGrid, GridCells
+from nav_msgs.msg import OccupancyGrid, GridCells, Path
 from Queue import PriorityQueue
-from enum import Enum
+from geometry_msgs.msg import PoseStamped
+#from enum import Enum
 
 #Helper class
 class Point(object):
@@ -25,7 +26,7 @@ class Point(object):
 #keep tracks of the state of each cell in the map
 class NavMap(object):
 	#possible cell states
-	class CellState (Enum):
+	class CellState (object):
 		free = 0
 		frontier = 1
 		explored = 2
@@ -33,17 +34,17 @@ class NavMap(object):
 	
 	def __init__(self, ocmapE, goalPoint): #ocmap = OccupancyGrid global_cost_map
 		
-		width = ocmap.info.width #just to make it easier to see
-		height = ocmap.info.height
+		self.width = ocmap.info.width #just to make it easier to see
+		self.height = ocmap.info.height
 		
 		#where we store the state of the cells
-		self.table = [[CellState.free]*width][height] #At first we fill it all with CellState.free
+		self.table = [[CellState.free]*self.width][self.height] #At first we fill it all with CellState.free
 		
 		self.goalPoint = goalPoint
 		
-		for(col in range(height)):
-			for(row in range(width)):
-				if(ocmap.data[col*width] > 90): #ocmap.data[] is a simple array
+		for row in range(self.height):
+			for col in range(self.width):
+				if(ocmap.data[row*self.width+col] > 90): #ocmap.data[] is a simple array
 					self.map[row][col] = CellState.blocked
 				# else it keeps with CellState.free
 	
@@ -53,19 +54,34 @@ class NavMap(object):
 		table[parent.pos.y][parent.pos.x] = CellState.explored
 		
 		children = [] #return list
-		for(i in range(-1,1)):
-			for(j in range(-1,1)):
+		for i in range(-1,1):
+			for j in range(-1,1):
 				#with abs(i)==abs(j) we just consider turns of 90 degreees
-				#trye i == j == 0 to include diagonals
-				if(abs(i)==abs(j) or table[i][j] != CellState.free):
-					continue
-				table[i][j] = CellState.frontier
+				#try i == j == 0 to include diagonals
+				
 				pos = parent.plus(Point(i,j)) #we can have problems with the definition of (i,j) (row,col) and (x,y) FIXME
+				
+				#FIXME: not good code
+				if(not self.inBounds(pos)):
+					continue
+				else:
+					if(abs(i)==abs(j) or table[pos.y][pos.x] != CellState.free):
+						continue
+				
+				table[pos.y][pos.x] = CellState.frontier
 				pointDiff = self.goalPoint.minus(parent.pos)
 				h_n = abs(pointDiff.x) + abs(pointDiff.y) #h_n = abs(dx) + abs(dy) #for 90 degree turn
+				
 				children.append(Node(pos, parent, h_n))
-					
+
 		return children
+	
+	
+	def inBounds(self,point):
+		if(point.x > 0 and point.x < self.width and point.y > 0 and point.y < self.height):
+			return True
+		else:
+			return False
 	
 	
 class Node(object):
@@ -86,92 +102,59 @@ class Node(object):
 	def cost():
 		return self.g_n + self.h_n
 		
-# reads in map data
-def mapCallBack(data):
-    global mapData, grid
-    global width
-    global height
-    global mapgrid
-    mapgrid = data
-    mapData = data.data
-    width = data.info.width
-    height = data.info.height
-    #print data.info # used for debugging
-    #publishCells(mapgrid.data) # used for debugging
 
 def globCostCallBack(data):
-    global mapData, globCostMap
-    global width
-    global height
-    global mapgrid
+    global globalMapGrid
+    global startPathPlanning
+    
     globMapGrid = data
+    startPathPlanning = True
 
-def locCostCallBack(data):
-    global mapData, locCostMap
-    global width
-    global height
-    global mapgrid
-    locMapGrid = data
-
-def publishGlobCostCells(globCostMap)
-    pass
-
-def publishLocCostCells(locCostMap)
-    pass
-
-def globPlanCallBack(data):
-	sequence = data.poses.seq	#sequence ID: consecutively increasing
-	data.poses.frame_id		#0: no frame, 1: global frame
-	pass
-
-def locPlanCallBack(data):
-	sequence = data.poses.seq	#sequence ID: consecutively increasing
-	ID = data.poses.frame_id		#0: no frame, 1: global frame
-	pass
-
-def publishCells(grid):
-    global pub
-    print "publishing"
-    k=0
-    cells = GridCells()
-    cells.header.frame_id = 'map'
-    cells.cell_width = 0.3 # edit for grid size .3 for simple map
-    cells.cell_height = 0.3 # edit for grid size
-
-    for i in range(1,height): #height should be set to height of grid
-        k=k+1
-        for j in range(1,width): #height should be set to height of grid
-            k=k+1
-            #print k # used for debugging
-            if (grid[k] == 100):
-                point=Point()
-                point.x=j*.3 # edit for grid size
-                point.y=i*.3 # edit for grid size
-                point.z=0
-                cells.cells.append(point)
-		
+def readGoal(msg):
+	global goalPoint
+	global newGoal
+	goalPoint = msg
+	newGoal = True
 		
 def run():
-    global pub
-    #rospy.init_node('lab3')
-   #mapSub = rospy.Subscriber("/map", OccupancyGrid, mapCallBack)
-    globCostSub = rospy.Subscriber("/move_base/global_costmap/costmap", OccupancyGrid, globCostCallBack)
-    #locCostSub = rospy.Subscriber("/move_base/local_costmap/costmap", OccupancyGrid, locCostCallBack) # Do not need yet
-    globPlanPub = rospy.Publisher("/aStarPath", Path, queue_size=1)
-    #locPlanSub = rospy.Subscriber("/move_base/DWAPlannerROS/local_plan", Path, locPlanCallBack)# Do not need yet
-    markerSub = rospy.Subscriber('/aStarNavGoal', PoseStamped, readGoal)
+	global goalPoint
+	global newGoal
+	global globalMapGrid
+	global startPathPlanning
+	
+	startPathPlanning = False
+	newGoal = False
+	
+	
+	rospy.init_node('lab3')
+	
+	globCostSub = rospy.Subscriber("/move_base/global_costmap/costmap", OccupancyGrid, globCostCallBack)
+	globPlanPub = rospy.Publisher("/aStarPath", Path, queue_size=1)
+	markerSub = rospy.Subscriber('/aStarNavGoal', PoseStamped, readGoal)
 
-    frontierPub = rospy.Publisher("/grid_Frontier", GridCells, queue_size=1)
-    exploredPub = rospy.Publisher("/grid_explored", GridCells, queue_size=1)
+	frontierPub = rospy.Publisher("/grid_Frontier", GridCells, queue_size=1)
+	exploredPub = rospy.Publisher("/grid_explored", GridCells, queue_size=1)
     
     
-    sleeper = rospy.Duration(1)
-    rospy.sleep(sleeper)
+	sleeper = rospy.Duration(1)
+	rospy.sleep(sleeper)
+    
+    
+    
+    
     
     #initialize shit
     
-    # 
-    
+	while not rospy.is_shutdown():
+    	
+    	if startPathPlanning and newGoal:
+    	
+    		map = NavMap(globMapGrid, goalPoint)
+    		#priority_queue stuff
+    		
+    		startPathPlanning = False
+    		newGoal = False
+    		
     
     
     rospy.spin()
