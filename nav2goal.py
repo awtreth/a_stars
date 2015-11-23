0a34333c0d5ce1759e8975cf2bcd53abe13b41d6
Mateus Amarante Araujo <mateus.amarujo@gmail.com>
2015-11-23 17:19:21 -0500

    moveTo working! :)

--- nav2goal.py
+++ nav2goal.py
@@ -12,8 +12,6 @@ from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Quaternion
 from nav_msgs.msg import Odometry
 
-base = .23
-
 #This function consumes linear and angular velocities
 #and creates a Twist message.  This message is then published.
 def publishTwist(lin_vel, ang_vel):
@@ -24,91 +22,6 @@ def publishTwist(lin_vel, ang_vel):
     twist_msg.linear.x = lin_vel	#Populate message with data
     twist_msg.angular.z = ang_vel
     pub.publish(twist_msg)		#Send Message
- 
-#This function accepts two wheel velocities and a time interval.
-def spinWheels(u1, u2, time):
-    global pub
-    
-
-    lin_vel = (u1+u2)/2			#Determines the linear velocity of base based on the wheels
-    ang_vel = (u1-u2)/base		#Determines the angular velocity of base on the wheels.
-
-    twist_msg = Twist();			#Creates two messages: 
-    stop_msg = Twist();
-
-    twist_msg.linear.x = lin_vel		#Populate messages with data.
-    twist_msg.angular.z = ang_vel
-    stop_msg.linear.x = 0
-    stop_msg.angular.z = 0
-    
-    #While the specified amount of time has not elapsed, send Twist messages.
-    now = rospy.Time.now().secs
-    while (rospy.Time.now().secs - now <= time and not rospy.is_shutdown()):
-        pub.publish(twist_msg)
-    pub.publish(stop_msg)
-    
-
-#This function accepts a speed and a distance for the robot to move in a straight line
-def driveStraight(speed, distance):
-    global pose 	
-
-    x0 = pose.pose.position.x	#Set origin
-    y0 = pose.pose.position.y
-
-    #Loop until the distance between the attached frame and the origin is equal to the
-    #distance specifyed 
-    done = False
-    while (not done and not rospy.is_shutdown()):
-        x1 = pose.pose.position.x
-        y1 = pose.pose.position.y
-        d = math.sqrt(math.pow(x1-x0, 2) + math.pow(y1-y0, 2))	#Distance formula
-        if (d >= distance):
-            done = True
-            publishTwist(0, 0)
-        else:
-            publishTwist(speed, 0)
-
-    
-#Accepts an angle and makes the robot rotate around it.
-def rotate(angle):
-    #global pose
-    global lst
-
-    #This node was created using Coordinate system transforms and numpy arrays.
-    #The goal is measured in the turtlebot's frame, transformed to the odom.frame 
-    transformer = tf.TransformerROS()	
-    rotation = numpy.array([[math.cos(angle), -math.sin(angle), 0],	#Create goal rotation
-                            [math.sin(angle), math.cos(angle), 0],
-                            [0,          0,          1]])
-
-    #Get transforms for frames
-    lst.waitForTransform('odom', 'base_footprint', rospy.Time(0), rospy.Duration(4.0))
-    (trans, rot) = lst.lookupTransform('odom', 'base_footprint', rospy.Time(0))
-    T_o_t = transformer.fromTranslationRotation(trans, rot)
-    R_o_t = T_o_t[0:3,0:3]
-
-    #Setup goal matrix
-    goal_rot = numpy.dot(rotation, R_o_t)
-    goal_o = numpy.array([[goal_rot[0,0], goal_rot[0,1], goal_rot[0,2], T_o_t[0,3]],
-                    [goal_rot[1,0], goal_rot[1,1], goal_rot[1,2], T_o_t[1,3]],
-                    [goal_rot[2,0], goal_rot[2,1], goal_rot[2,2], T_o_t[2,3]],
-                    [0,             0,             0,             1]])
-
-    #Continues creating and matching coordinate transforms.
-    done = False
-    while (not done and not rospy.is_shutdown()):
-        (trans, rot) = lst.lookupTransform('odom', 'base_footprint', rospy.Time(0))
-        state = transformer.fromTranslationRotation(trans, rot)
-        within_tolerance = abs((state - goal_o)) < .275
-        if ( within_tolerance.all() ):
-            spinWheels(0,0,0)
-            done = True
-        else:
-            if (angle > 0):
-                spinWheels(.1,-.1,.1)
-            else:
-                spinWheels(-.1,.1,.1)
-
 
 #drive to a goal subscribed as /move_base_simple/goal
 def navToPose(goal):
@@ -133,22 +46,27 @@ def navToPose(goal):
 	(trans, rot) = lst.lookupTransform("base_footprint", "goal", rospy.Time(0)) #get the transform between odom and goal
 	
 	#angle between the first orientation and the final one
-	totalAngle = tf.transformations.euler_from_quaternion(rot)[2]
-	angle1 = math.atan2(trans[1], trans[0]) #first turn angle
-	angle2 = from360to180(totalAngle - angle1) #second turn angle
+	#totalAngle = tf.transformations.euler_from_quaternion(rot)[2]
+	baseGoalAngle = math.atan2(trans[1], trans[0]) #first turn angle
 	
-	distance = math.hypot(trans[0], trans[1]) #distance between points (hypo calculates the hypotenuse)
+	#Wait until the following transformation is possible
+	lst.waitForTransform("map", "base_footprint", rospy.Time(), rospy.Duration(2))
+	#TODO: Exception handler
+	(trans, rot) = lst.lookupTransform("map", "base_footprint", rospy.Time(0)) #get the transform between odom and goal
 	
-	print "spin1 %f"%math.degrees(angle1)
-	rotate(angle1)
+	baseAngle = tf.transformations.euler_from_quaternion(rot)[2]
+	
+	turn1angle = from360to180(baseAngle + baseGoalAngle)
+	turn2angle = yawFromQuatMsg(goal.pose.orientation)
+	
+	
+	print "turn1angle = " + repr(math.degrees(turn1angle))
+	turnTo(turn1angle, math.radians(5))
+	moveTo(goal.pose.position.x, goal.pose.position.y, .1)
+	print "turn2angle = " + repr(math.degrees(turn2angle))
+	turnTo(turn2angle, math.radians(5))
+	print "done"
 
-	print "move %f"%distance
-	driveStraight(.35,distance)
-	
-	print "spin2 %f"%math.degrees(angle2)
-	rotate(angle2)
-
-	print "done!"
 
 #this function covert an angle from -360~360 to -180~180
 #Examples: 190 -> -170 and -190 -> 170
@@ -188,15 +106,15 @@ def turnTo(goalAngle, tolerance):
 		
 		currentDiff = tf.transformations.euler_from_quaternion(rot)[2]
 		
-		print "currentDiff = " + repr(currentDiff)
+		#print "currentDiff = " + repr(currentDiff)
 		
 		if(abs(currentDiff) <= tolerance):
 			publishTwist(0,0);
-			print "break"
+			print "break turn"
 			break
 		
 		w = currentDiff*constant
-		print "w = " + repr(w)
+		#print "w = " + repr(w)
 		abs_w = abs(w)
 		if(abs_w > max_vel): w = math.copysign(max_vel,w)
 		elif(abs_w < min_vel): w = math.copysign(min_vel,w)
@@ -204,20 +122,47 @@ def turnTo(goalAngle, tolerance):
 		publishTwist(0,w);
 	
 
-def moveTo(x,y):
+def moveTo(x,y, tolerance):
 	global lst
 	global br
 	
-	
+	moveToTime = rospy.Time.now()
 	#Create the goal frame, related to the map
-	br.sendTransform((x,y,0),(0,0,0,1),rospy.Time().now(), "moveTo", "map")
+	br.sendTransform((x,y,0),(0,0,0,1),moveToTime, "move_to", "map")
 	
-	#Wait until the following transformation is possible
-	lst.waitForTransform("base_footprint", "goal", rospy.Time(), rospy.Duration(2))
-	#TODO: Exception handler
-	(trans, rot) = lst.lookupTransform("base_footprint", "goal", rospy.Time(0)) #get the transform between odom and goal
+	kp_w = .5
+	max_w = .3
+	kp_vel = .5
+	max_vel = .4
+	min_vel = .15
 	
-	
+	while not rospy.is_shutdown():
+		lst.waitForTransformFull("base_footprint", rospy.Time(0), "move_to", moveToTime, "map", rospy.Duration(2))
+		#TODO: Exception handler
+		(trans, rot) = lst.lookupTransformFull("base_footprint", rospy.Time(0), "move_to", moveToTime,"map") #get the transform between odom and goal
+		dist = math.hypot(trans[0], trans[1])
+		#print "dist = " + repr(dist)
+		
+		if(dist <= tolerance):
+			print "break move"
+			publishTwist(0,0)
+			break
+		
+		vel = dist*kp_vel
+		#print "vel1 = " + repr(vel)
+		if(vel > max_vel): vel = max_vel
+		elif(vel < min_vel): vel = min_vel
+		#print "vel2 = " + repr(vel)
+		
+		angle = math.atan2(trans[1], trans[0]) #first turn angle
+		#print "angle = " + repr(angle)
+		w = kp_w*angle
+		#print "w1 = " + repr(w)
+		if(abs(w) > max_w): w = math.copysign(max_w,w)
+		#print "w2 = " + repr(w)
+		
+		publishTwist(vel,w)
+		
 	
 #Odometry Callback function.
 def readOdom(msg):
@@ -251,7 +196,4 @@ if __name__ == '__main__':
 	
 	time.sleep(1)
 	
-	turnTo(math.radians(180), math.radians(1))
-	turnTo(math.radians(90), math.radians(1))
-
 	rospy.spin()
