e700efe8cc12d34c6766b5a865dbfdcae0e4b80a
Mateus Amarante Araujo <mateus.amarujo@gmail.com>
2015-12-06 16:18:19 -0500

    started testing

--- final_main.py
+++ final_main.py
@@ -18,36 +18,37 @@ class Point2D(object):
 class CustomMap(object):
 	#Possible states for each position of the map
 	FREE = 0
-	UNKNOWN = -1
+	UNKNOWN = 2
 	OBSTACLE = 1
 	
 	#Default Constructor
-	def __init__(self, globalMap, localMap, threshold = 99):
+	def __init__(self, globalMap, localMap, threshold = 90):
 		
 		self.setupPubTopics()
 
 		self.width = globalMap.info.width
 		self.height = globalMap.info.height
-		self.map = [self.height][self.width]
+		self.mmap = [[self.UNKNOWN]*self.width]*self.height
+		#it assumes that the orientation of the globalCostMap doesn't change
+		self.origin = Point2D(globalMap.info.origin.position.x, globalMap.info.origin.position.y)
+		self.resolution = globalMap.info.resolution
 
 	#create the state table
-		for row in range(self.height):
-			offset = row*self.width
-			for col in range(self.width):
-				idx = offset+col
-				if(globalMap.data[idx] >= threshold):
-					self.map[row][col] = self.OBSTACLE #set the obstacles
-				elif globalMap.data[idx] == -1:
-						self.map[row][col] = self.UNKNOWN
+		for y in range(0,self.height):
+			offset = y*self.width
+			for x in range(0,self.width):
+				idx = y*self.width+x
+				if globalMap.data[idx] >= threshold:
+					#print "obstacle " + repr(x) + ' ' + repr(y)
+					self.set(x,y,self.OBSTACLE) #set the obstacles
 				else:
-					self.map[row][col] = self.FREE
+					#print "free " + repr(x) + ' ' + repr(y)
+					self.set(x,y,self.FREE)
+				#if (idx%100 is 0):
+				#	raw_input('press Enter')
 		
+		#print self.mmap
 		self.update(localMap)
-
-	def setupPubTopics(self):
-		pass
-
-
 
 	# Update the map based on a new localMap
 	# Keep the globalMap as it was before
@@ -56,15 +57,15 @@ class CustomMap(object):
 
 	#Get the state of the specified position
 	def get(self,x,y):
-		if inBounds(x,y):
-			return self.map[y][x]
+		if self.inBounds(x,y):
+			return self.mmap[y][x]
 		else:
 			print "Not inBounds in CustomMap::get method" #TODO: throw exception
 
 	#Set the state of the specified position
 	def set(self,x,y, value):
-		if inBounds(x,y):
-			self.map[y][x] = value #TODO: check if value is valid
+		if self.inBounds(x,y):
+			self.mmap[y][x] = value #TODO: check if value is valid
 		else:
 			print "Not inBounds in CustomMap::set method" #TODO: throw exception
     
@@ -96,10 +97,49 @@ class CustomMap(object):
 				points.append(Point2D(xx,yy))
 		
 		return points
-	  
-		
+	
+	def setupPubTopics(self):
+		self.unknownPub = rospy.Publisher("/unknown_area", GridCells, queue_size=1)
+		self.freePub = rospy.Publisher("/free_area", GridCells, queue_size=1)
+		self.obstaclePub = rospy.Publisher("/obstacle_area", GridCells, queue_size=1)
+
 	def publishMap(self):
-		pass
+		self.unknownPub.publish(self.getGridCell(self.UNKNOWN))
+		self.freePub.publish(self.getGridCell(self.FREE))
+		self.obstaclePub.publish(self.getGridCell(self.OBSTACLE))
+	
+	
+	def printMap(self):
+		for y in range(self.height):
+			line = []
+			for x in range(self.width):
+				line.append(self.get(x,y))
+			
+			print line
+	
+	
+	
+	# Return the GridCells ROS msg of the cells with state "value"
+	def getGridCell(self, value):
+		
+		#self.printMap()
+		
+		grid = GridCells()
+		grid.header.frame_id = "map"
+		grid.cell_width = self.resolution
+		grid.cell_height = self.resolution
+		
+		for y in range(0,self.height):
+			for x in range(0,self.width):
+				if(self.get(x,y) == value):
+					
+					point = Point()
+					point.x = x*self.resolution+self.origin.x
+					point.y = y*self.resolution+self.origin.y
+					point.z = 0
+					grid.cells.append(point)
+					
+		return grid
 
 class FrontierProcessor(object):
 	def __init__(self):
@@ -127,24 +167,36 @@ def readStatus(rosStatus):
 #Callback function that gets the globalCostMap
 def readGlobalMap(rosmap):
 	global globalMap
+	globalMap = rosmap
+	#print "global"
 
 #Callback function that gets the localCostMap
 def readLocalMap(rosmap):
 	global localMap
+	localMap = rosmap
+	#print "local"
 
 #Main Function
 if __name__ == '__main__':
 	global status
+	global localMap
+	global globalMap
 
-	rospy.Subscriber('/move_base/status', GoalStatusArray, readStatus)
+	rospy.init_node('final_main')
+
+	#rospy.Subscriber('/move_base/status', GoalStatusArray, readStatus)
 	rospy.Subscriber('/move_base/global_costmap/costmap', OccupancyGrid, readGlobalMap)
 	rospy.Subscriber('/move_base/local_costmap/costmap', OccupancyGrid, readLocalMap)
+
 
 	rospy.sleep(1)
 
 	mmap = CustomMap(globalMap, localMap)    
-
-
+	
+	
+	while True:
+		mmap.publishMap()
+		rospy.sleep(1)
 	rospy.spin()
 
 
